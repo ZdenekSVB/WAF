662979dccc57755ad496e79f368b017c9454da34
Marian Molnar <xmolnar2@mendelu.cz>
2024-06-23 21:17:45 +0200

    title pro vsechny stranky

--- vuetify-project/src/main.ts
+++ vuetify-project/src/main.ts
@@ -1,7 +1,7 @@
 /**
  * main.ts
  *
- * Bootstraps Vuetify and other plugins then mounts the App`
+ * Bootstraps Vuetify and other plugins then mounts the App
  */
 
 // Plugins
@@ -13,8 +13,22 @@ import App from './App.vue'
 // Composables
 import { createApp } from 'vue'
 
+// Import the router
+import router from './router'
+
 const app = createApp(App)
 
+// Register plugins
 registerPlugins(app)
 
+// Add the router.beforeEach hook
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || 'Game Statistics'
+  next()
+})
+
+// Use the router
+app.use(router)
+
+// Mount the app
 app.mount('#app')
