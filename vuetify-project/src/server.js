89218b2e77025641e277b8344d7a0cfc0761e67a
xcevelik <xcevelik@mendelu.cz>
2024-06-24 23:20:00 +0200

    server

--- vuetify-project/src/server.js
+++ vuetify-project/src/server.js
@@ -8,12 +8,13 @@ const PORT = 3002;
 app.use(cors());
 app.use(express.json());
 
-const API_KEY = 'RGAPI-ec61a056-956b-4ebc-808a-3a419bedbda9';
+//LOL
+const API_KEY = 'RGAPI-9dd55f0b-1a95-4b68-8bf1-1d88abbbf2af';
 const ACCOUNT_API_ROUTING_VAL = "https://europe.api.riotgames.com"; 
 const SUMMONER_API_ROUTING_VAL = "https://eun1.api.riotgames.com"; 
 
 
-// Endpoint for fetching PUUID by Riot ID
+
 app.get('/api/getPUUID/:gameName/:tagLine', async (req, res) => {
   const { gameName, tagLine } = req.params;
   try {
@@ -30,7 +31,7 @@ app.get('/api/getPUUID/:gameName/:tagLine', async (req, res) => {
       res.status(500).json({ error: 'Error fetching PUUID', details: error.response ? error.response.data : error.message });
   }
 });
-// Endpoint for fetching summoner data by PUUID
+
 app.get('/api/getSummonerData/:puuid', async (req, res) => {
   const { puuid } = req.params;
   try {
@@ -59,7 +60,7 @@ app.get('/api/getSummonerData/:puuid', async (req, res) => {
   }
 });
 
-// Endpoint for fetching match history by PUUID
+
 app.get('/api/getMatchHistory/:puuid', async (req, res) => {
   const { puuid } = req.params;
   const { start = 0 } = req.query;
@@ -136,7 +137,6 @@ async function fetchMatchDetailsWithDelay(matchIds, puuid, ACCOUNT_API_ROUTING_V
   return matchDetails;
 }
 
-// Endpoint for fetching league entries by Summoner ID
 app.get('/api/getLeagueEntries/:summonerId', async (req, res) => {
   const { summonerId } = req.params;
   try {
@@ -156,6 +156,8 @@ app.get('/api/getLeagueEntries/:summonerId', async (req, res) => {
 
 
 
+
+//FACEIT
 const FACEIT_API_KEY = '70e13787-dfaa-4e89-8e92-fde2a100d028';
 const FACEIT_API_URL = 'https://open.faceit.com/data/v4';
 
@@ -220,6 +222,136 @@ app.get('/api/getFaceitMatches/:playerId', async (req, res) => {
     res.status(500).json({ error: 'Error fetching FACEIT matches' });
   }
 });
+
+
+
+//TFT
+app.get('/api/summoner/:name/:tag', async (req, res) => {
+  const { name, tag } = req.params;
+  const region =tag.toLowerCase()
+  const accountUrl = `https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${name}/${tag}?api_key=${API_KEY}`;
+
+  try {
+    const accountResponse = await axios.get(accountUrl);
+    console.log('Initial Summoner Data Response:', accountResponse.data);
+    const puuid = accountResponse.data.puuid;
+
+    const tftSummonerUrl = `https://${region}.api.riotgames.com/tft/summoner/v1/summoners/by-puuid/${puuid}?api_key=${API_KEY}`;
+    const tftSummonerResponse = await axios.get(tftSummonerUrl);
+    console.log('TFT Summoner Data Response:', tftSummonerResponse.data);
+
+    const summonerId = tftSummonerResponse.data.id;
+    const leagueUrl = `https://${region}.api.riotgames.com/tft/league/v1/entries/by-summoner/${summonerId}?api_key=${API_KEY}`;
+    const leagueResponse = await axios.get(leagueUrl);
+    console.log('League Data Response:', leagueResponse.data);
+
+    const matchUrl = `https://europe.api.riotgames.com/tft/match/v1/matches/by-puuid/${puuid}/ids?start=0&count=3&api_key=${API_KEY}`;
+    const matchResponse = await axios.get(matchUrl);
+    console.log('Match IDs Response:', matchResponse.data);
+
+    const matchDetailsPromises = matchResponse.data.map(async (matchId, index) => {
+      const matchDetailsUrl = `https://europe.api.riotgames.com/tft/match/v1/matches/${matchId}?api_key=${API_KEY}`;
+      const matchDetailsResponse = await axios.get(matchDetailsUrl);
+
+      const participant = matchDetailsResponse.data.info.participants.find(p => p.puuid === puuid);
+      if (participant) {
+        const filteredParticipant = {
+          game_number: index + 1, // Přidání čísla hry
+          augments: participant.augments, // Only include augment names
+          gold_left: participant.gold_left,
+          last_round: participant.last_round,
+          level: participant.level,
+          missions: participant.missions,
+          placement: participant.placement,
+          players_eliminated: participant.players_eliminated,
+          puuid: participant.puuid,
+          time_eliminated: participant.time_eliminated,
+          total_damage_to_players: participant.total_damage_to_players,
+        };
+
+        console.log(`Participant in match ${matchId}:`, filteredParticipant);
+      } else {
+        console.error(`Participant with PUUID ${puuid} not found in match ${matchId}`);
+      }
+
+      return {
+        matchId: matchId,
+        matchDetails: matchDetailsResponse.data,
+      };
+    });
+
+    const matchDetails = await Promise.all(matchDetailsPromises);
+
+    const summonerData = {
+      gameName: accountResponse.data.gameName,
+      tagLine: accountResponse.data.tagLine,
+      summonerLevel: tftSummonerResponse.data.summonerLevel,
+      profileIconId: tftSummonerResponse.data.profileIconId,
+      leagueData: leagueResponse.data,
+      matchIds: matchResponse.data,
+      matchDetails: matchDetails,
+      puuid: puuid,
+    };
+
+    res.json(summonerData);
+  } catch (error) {
+    console.error('Error:', error.response ? error.response.data : error.message);
+    res.status(500).send(error.response ? error.response.data : error.message);
+  }
+});
+
+
+app.get('/api/leaderboard/:region/:tier/:division', async (req, res) => {
+  const { region, tier, division } = req.params;
+  const validRegions = ['eun1', 'euw1', 'br1', 'jp1', 'kr', 'la1', 'la2','na1', 'oc1', 'tr1', 'ru', 'ph2', 'sg2', 'th2','tw2', 'vn2'];  
+  const validTiers = ['IRON', 'BRONZE', 'SILVER', 'GOLD', 'PLATINUM', 'DIAMOND'];
+  const validDivisions = ['I', 'II', 'III', 'IV'];
+
+  if (!validRegions.includes(region) || !validTiers.includes(tier) || !validDivisions.includes(division)) {
+    return res.status(400).json({ message: 'Invalid region, tier, or division' });
+  }
+
+  const leagueUrl = `https://${region}.api.riotgames.com/tft/league/v1/entries/${tier}/${division}?queue=RANKED_TFT&page=1&api_key=${API_KEY}`;
+
+  try {
+    const leagueResponse = await axios.get(leagueUrl);
+    const players = leagueResponse.data.slice(0, 20);
+    
+    console.log('League Data Response:', leagueResponse.data);
+    
+    const summonerPromises = players.map(async player => {
+      const summonerUrl = `https://europe.api.riotgames.com/riot/account/v1/accounts/by-puuid/${player.puuid}?api_key=${API_KEY}`;
+      const summonerResponse = await axios.get(summonerUrl);
+      return {
+        summonerName: summonerResponse.data.gameName,
+        tier: player.tier,
+        rank: player.rank,
+        leaguePoints: player.leaguePoints,
+        wins: player.wins,
+        losses: player.losses
+      };
+    });
+
+    const summonerData = await Promise.all(summonerPromises);
+    res.json(summonerData);
+  } catch (error) {
+    console.error('Error:', error.response ? error.response.data : error.message);
+    res.status(500).send(error.response ? error.response.data : error.message);
+  }
+});
+
+//DOTA
+app.get('/api/players/:account_id', async (req, res) => {
+  const { account_id } = req.params;
+  try {
+    const response = await axios.get(`https://api.opendota.com/api/players/${account_id}`);
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching player data:', error.response ? error.response.data : error.message);
+    res.status(500).json({ error: 'Error fetching player data', details: error.response ? error.response.data : error.message });
+  }
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
